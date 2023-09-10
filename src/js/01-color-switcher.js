bc56ffe33bd9ca94599d81d3e89d53181548e7cb
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2023-09-10 21:15:32 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const timer = 1000;
+
+console.log(btnStart);
+console.log(btnStop);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+btnStart.addEventListener('click', generatorColorStart);
+
+
+function generatorColorStart (event){
+
+const timerId = setTimeout(() => {
+
+    document.body.nodeValue = getRandomHexColor()
+    
+
+}, timer);
+}
 
